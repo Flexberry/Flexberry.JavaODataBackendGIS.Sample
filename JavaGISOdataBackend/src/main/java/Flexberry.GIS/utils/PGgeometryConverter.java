7b968e5eeea7208f77a0d2ccac1be37387b07de1
AntonIV8 <anton-iv87@yandex.ru>
2023-02-13 14:40:30 +0500

    Добавлена конвертация из String в PGgeometry, для записи в бд

--- JavaGISOdataBackend/src/main/java/Flexberry.GIS/utils/PGgeometryConverter.java
+++ JavaGISOdataBackend/src/main/java/Flexberry.GIS/utils/PGgeometryConverter.java
@@ -4,20 +4,23 @@ import org.eclipse.persistence.internal.helper.DatabaseField;
 import org.eclipse.persistence.mappings.DatabaseMapping;
 import org.eclipse.persistence.mappings.converters.Converter;
 import org.eclipse.persistence.sessions.Session;
+import org.json.JSONObject;
+import org.postgis.Geometry;
 import org.postgis.PGgeometry;
 
-import java.sql.Types;
+import java.sql.SQLException;
 
 public class PGgeometryConverter implements Converter {
     private static final long serialVersionUID = 4516918363697406455L;
 
     @Override
     public PGgeometry convertObjectValueToDataValue(Object objectValue, Session session) {
-        if ((null == objectValue) || (null == session) || (!(objectValue instanceof org.postgis.Geometry)))
+        if (objectValue == null || objectValue.toString().equals("") || null == session)
             return null;
 
-        // TODO тут нужно будет брать строку JSON и создавать из нее тип PGgeometry.
-        return new org.postgis.PGgeometry((org.postgis.Geometry)objectValue);
+        JSONObject geometryJson = new JSONObject((String) objectValue);
+
+        return ConvertGISJsonToPGgeometry(geometryJson);
     }
 
 
@@ -30,7 +33,6 @@ public class PGgeometryConverter implements Converter {
         return "";
     }
 
-
     @Override
     public boolean isMutable() {
         return false;
@@ -40,7 +42,47 @@ public class PGgeometryConverter implements Converter {
     public void initialize(DatabaseMapping mapping, Session session) {
         DatabaseField field = mapping.getField();
         field.setSqlType(java.sql.Types.OTHER);
-        field.setTypeName("java.lang.String");
+        field.setTypeName("org.postgis.PGgeometry");
         field.setColumnDefinition("GEOGRAPHY");
+    }
+
+    private PGgeometry ConvertGISJsonToPGgeometry(JSONObject gisJson) {
+        String srid = "SRID=4326";
+
+        if (gisJson.has("crs")) {
+            JSONObject crsFromJson =  gisJson.getJSONObject("crs");
+            if (crsFromJson.has("properties")) {
+                JSONObject propertiesJson = crsFromJson.getJSONObject("properties");
+                if (propertiesJson.has("name")) {
+                    String stringEPSGValue = propertiesJson.get("name").toString();
+                    srid = stringEPSGValue.replace("EPSG:", "SRID=");
+                }
+            }
+        }
+
+        String geometryType = "PONT";
+
+        if (gisJson.has("type")) {
+            geometryType = gisJson.get("type").toString().toUpperCase();
+        }
+
+        String coordinates = "(0 0)";
+
+        if (gisJson.has("coordinates")) {
+            coordinates = gisJson.get("coordinates").toString();
+            coordinates = coordinates.replaceAll("\\[", "(").replaceAll("\\]",")");
+            coordinates = coordinates.replaceAll(",", " ").replaceAll("\\) \\(", "),(");
+        }
+
+        String geometryStringForPostgis = srid + ";" + geometryType + coordinates;
+
+        try {
+            Geometry geometryObject = PGgeometry.geomFromString(geometryStringForPostgis);
+            PGgeometry postgisGeometryObject = new PGgeometry(geometryObject);
+
+            return postgisGeometryObject;
+        } catch (SQLException e) {
+            return null;
+        }
     }
 }
